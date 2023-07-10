8a064988338722712455ec0ce330b4d135164299
Michael Heckmann <123197662+MichaelHeckmann83@users.noreply.github.com>
2023-07-10 07:02:20 +0200

    Update main.py
    data autofill switches off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import TimeManagement
 
-me = TimeManagement.LabourTime.LabourTime.create_employee(data=True)
+me = TimeManagement.LabourTime.LabourTime.create_employee()
 vis = TimeManagement.Visualizer.Visualizer.build_visualizer(me)
 vis.show_table_pretty()
